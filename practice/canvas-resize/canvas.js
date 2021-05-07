53db38dd131c83f63915984381a2eba613918c60
Michael Pingel <mdpingel@gmail.com>
2021-05-07 14:27:34 -0700

    follow along with html5 canvas tutorial for beginners

--- practice/canvas-resize/canvas.js
+++ practice/canvas-resize/canvas.js
@@ -1,33 +1,33 @@
 // shouldn't use var
-var canvas = document.querySelector('canvas');
+const canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 // c is short for context
-var c = canvas.getContext('2d');
+const c = canvas.getContext('2d');
 
               // fillStyle colors all rectangles (fills?) below it
               // can be rgb, hex, or text color
-c.fillStyle = 'rgba(255, 0, 0, 0.5)';
+// c.fillStyle = 'rgba(255, 0, 0, 0.5)';
           // x & y are from top-left
           // x, y, width, height
-c.fillRect(100, 100, 100, 100);
-c.fillStyle = 'rgba(0, 255, 0, 0.5)';
-c.fillRect(400, 100, 100, 100);
-c.fillStyle = 'rgba(0, 0, 255, 0.5)';
-c.fillRect(50, 50, 20, 20);
-c.fillStyle = 'rgba(123, 123, 321, 0.5)';
-c.fillRect(500, 300, 123, 48);
-console.log(canvas);
+// c.fillRect(100, 100, 100, 100);
+// c.fillStyle = 'rgba(0, 255, 0, 0.5)';
+// c.fillRect(400, 100, 100, 100);
+// c.fillStyle = 'rgba(0, 0, 255, 0.5)';
+// c.fillRect(50, 50, 20, 20);
+// c.fillStyle = 'rgba(123, 123, 321, 0.5)';
+// c.fillRect(500, 300, 123, 48);
+// console.log(canvas);
 
 // Line
-c.beginPath();
+// c.beginPath();
       // x, y
-c.moveTo(50, 300);
-c.lineTo(300, 100);
-c.lineTo(400, 300);
-c.strokeStyle = "#fa34a3";
-c.stroke();
+// c.moveTo(50, 300);
+// c.lineTo(300, 100);
+// c.lineTo(400, 300);
+// c.strokeStyle = "#fa34a3";
+// c.stroke();
 
 // Arc / Circle
 // c.beginPath();
@@ -37,14 +37,81 @@ c.stroke();
 // c.stroke();
 
 
-for (let i = 0; i < 100; i++) {
-  const x = Math.random() * window.innerWidth;
-  const y = Math.random() * window.innerHeight;
+// for (let i = 0; i < 100; i++) {
+//   const x = Math.random() * window.innerWidth;
+//   const y = Math.random() * window.innerHeight;
+//   const r = Math.floor(Math.random() * 256);
+//   const g = Math.floor(Math.random() * 256);
+//   const b = Math.floor(Math.random() * 256);
+//   c.beginPath();
+//   c.arc(x, y, 30, 0, Math.PI * 2, false);
+//   c.strokeStyle = `rgba(${r}, ${g}, ${b}, 1)`;
+//   c.stroke();
+// }
+
+// let x = Math.random() * innerWidth;
+// let y = Math.random() * innerHeight;
+// let dx = (Math.random() - 0.5) * 8;
+// let dy = (Math.random() - 0.5) * 8;
+// let radius = 30;
+
+function Circle(x, y, dx, dy, radius, r, g, b) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+  this.r = r;
+  this.b = b;
+  this.g = g;
+
+  this.draw = function() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.strokeStyle = 'transparent';
+    c.stroke();
+    c.fillStyle = `rgba(${this.r}, ${this.g}, ${this.b}, 1)`;
+    c.fill();
+  }
+
+  this.update = function() {
+    if(this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    }
+    if(this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
+  
+    this.x += this.dx;
+    this.y += this.dy;
+
+    this.draw();
+  }
+}
+
+let circleArray = [];
+
+for (let i = 0; i < 500; i++) {
+  const radius = 30;
+  const x = Math.random() * (innerWidth - radius * 2) + radius;
+  const y = Math.random() * (innerHeight - radius * 2) + radius;
+  const dx = (Math.random() - 0.5);
+  const dy = (Math.random() - 0.5);
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
-  c.beginPath();
-  c.arc(x, y, 30, 0, Math.PI * 2, false);
-  c.strokeStyle = `rgba(${r}, ${g}, ${b}, 1)`;
-  c.stroke();
+  circleArray.push(new Circle(x, y, dx, dy, radius, r, g, b));
 }
+
+
+function animate() {
+  requestAnimationFrame(animate);
+  // same as fillRect but clears it
+  c.clearRect(0, 0, innerWidth, innerHeight);
+
+  for (let i = 0; i < circleArray.length; i++) {
+    circleArray[i].update();
+  }
+}
+
+animate();
