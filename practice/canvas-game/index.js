061a3c23245c82329952a3499c8d4d1a766e8490
Michael Pingel <mdpingel@gmail.com>
2021-05-14 11:03:55 -0700

    following along with game tutorial to learn about capturing user input

--- practice/canvas-game/index.js
+++ practice/canvas-game/index.js
@@ -43,55 +43,93 @@ class Projectile {
   }
 }
 
-const x = canvas.width / 2;
-const y = canvas.height / 2;
-
-const player = new Player(x, y, 30, 'blue');
-
-const projectile = new Projectile(
-  x,
-  y,
-  5,
-  'red',
-  {
-    x: 1,
-    y: 1
+class Enemy {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
   }
-)
 
-const projectile2 = new Projectile(
-  x,
-  y,
-  5,
-  'green',
-  {
-    x: -1,
-    y: -1
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
   }
-)
 
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
+
+const originX = canvas.width / 2;
+const originY = canvas.height / 2;
+
+const player = new Player(originX, originY, 30, 'blue');
 const projectiles = [];
+const enemies = [];
+
+function spawnEnemies() {
+  setInterval(() => {
+    const radius = Math.random() * (30 - 10) + 10;
+    let x;
+    let y;
+
+    if (Math.random() < 0.5) {
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+      y = Math.random() * canvas.height;
+    } else {
+      x = Math.random() * canvas.width;
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+    }
+
+    const color = 'green';
+    const angle = Math.atan2(originY - y, originX - x);
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle)
+    }
+    enemies.push(new Enemy(x, y, radius, color, velocity))
+  }, 1000)
+}
 
 player.draw();
 
 function animate() {
   requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  player.draw();
   projectiles.forEach((projectile) => {
     projectile.update();
+  })
+  enemies.forEach((enemy, enemyIndex) => {
+    enemy.update();
+
+    projectiles.forEach((projectile, projectileIndex) => {
+      const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
+
+      if (dist - enemy.radius - projectile.radius < 1) {
+        setTimeout(() => {
+          enemies.splice(enemyIndex, 1);
+          projectiles.splice(projectileIndex, 1);
+        }, 0)
+      }
+    });
   })
 }
 
 addEventListener('click', (event) => {
-  projectiles.push(new Projectile(
-    x,
-    y,
-    5,
-    'red',
-    {
-      x: 1,
-      y: 1
-    }
-  ))
+  const angle = Math.atan2(event.clientY - originY, event.clientX - originX);
+  const velocity = {
+    x: Math.cos(angle),
+    y: Math.sin(angle)
+  }
+  projectiles.push(new Projectile(originX, originY, 5, 'red', velocity))
 })
 
 animate();
+spawnEnemies();
