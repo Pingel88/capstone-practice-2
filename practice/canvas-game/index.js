f70cd9a3655c58a31f637a6f31685e3c910c25cb
Michael Pingel <mdpingel@gmail.com>
2021-05-14 14:12:35 -0700

    following along with game tutorial to learn about capturing user input

--- practice/canvas-game/index.js
+++ practice/canvas-game/index.js
@@ -69,13 +69,13 @@ class Enemy {
 const originX = canvas.width / 2;
 const originY = canvas.height / 2;
 
-const player = new Player(originX, originY, 30, 'blue');
+const player = new Player(originX, originY, 10, 'white');
 const projectiles = [];
 const enemies = [];
 
 function spawnEnemies() {
   setInterval(() => {
-    const radius = Math.random() * (30 - 10) + 10;
+    const radius = Math.random() * (50 - 10) + 10;
     let x;
     let y;
 
@@ -87,7 +87,7 @@ function spawnEnemies() {
       y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
     }
 
-    const color = 'green';
+    const color = `hsl(${Math.random() * 360}, 50%, 50%)`;
     const angle = Math.atan2(originY - y, originX - x);
     const velocity = {
       x: Math.cos(angle),
@@ -102,10 +102,16 @@ player.draw();
 let animationId
 function animate() {
   animationId = requestAnimationFrame(animate);
-  c.clearRect(0, 0, canvas.width, canvas.height);
+  c.fillStyle = 'rgba(0, 0, 0, 0.1)';
+  c.fillRect(0, 0, canvas.width, canvas.height);
   player.draw();
   projectiles.forEach((projectile, projectileIndex) => {
-    if (projectile.x - projectile.radius < 0) {
+    if (
+      projectile.x + projectile.radius < 0 ||
+      projectile.x - projectile.radius > canvas.width ||
+      projectile.y + projectile.radius < 0 ||
+      projectile.y - projectile.radius > canvas.height
+    ) {
       setTimeout(() => {
         projectiles.splice(projectileIndex, 1)
       }, 0);
@@ -116,18 +122,29 @@ function animate() {
     enemy.update();
     const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y);
 
-    if (dist - enemy.radius - player.radius < 1) {
-      cancelAnimationFrame(animationId);
-    }
+    // if (dist - enemy.radius - player.radius < 1) {
+    //   cancelAnimationFrame(animationId);
+    // }
 
     projectiles.forEach((projectile, projectileIndex) => {
       const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
 
+      //when projectiles touch enemy
       if (dist - enemy.radius - projectile.radius < 1) {
-        setTimeout(() => {
-          enemies.splice(enemyIndex, 1);
-          projectiles.splice(projectileIndex, 1);
-        }, 0)
+        if (enemy.radius - 10 > 5) {
+          gsap.to(enemy, {
+            radius: enemy.radius - 10
+          });
+          enemy.radius -= 10;
+          setTimeout(() => {
+            projectiles.splice(projectileIndex, 1);
+          }, 0)
+        } else {
+          setTimeout(() => {
+            enemies.splice(enemyIndex, 1);
+            projectiles.splice(projectileIndex, 1);
+          }, 0)
+        }
       }
     });
   })
@@ -136,11 +153,10 @@ function animate() {
 addEventListener('click', (event) => {
   const angle = Math.atan2(event.clientY - originY, event.clientX - originX);
   const velocity = {
-    x: Math.cos(angle),
-    y: Math.sin(angle)
+    x: Math.cos(angle) * 5,
+    y: Math.sin(angle) * 5
   }
-  projectiles.push(new Projectile(originX, originY, 5, 'red', velocity))
-  console.log(projectiles.length);
+  projectiles.push(new Projectile(originX, originY, 5, 'white', velocity))
 })
 
 animate();
